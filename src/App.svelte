bc880f6a9c723755ecbe7befaae3cecc9a65c197
Varun Jain <varunjainvj1@gmail.com>
2024-04-01 20:34:59 +0400

    tutorial

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,103 @@
 <script>
+	import data from './data/data.js';
+	import { scaleLinear } from 'd3-scale';
+	import { max } from 'd3-array';
+
+	import AxisX from './components/AxisX.svelte';
+	import AxisY from './components/AxisY.svelte';
+	import Tooltip from './components/Tooltip.svelte';
+
+	let width = 400;
+	let height = 400;
+
+	const margin = { top: 20, right: 40, left: 10, bottom: 20 };
+
+	$: xScale = scaleLinear()
+		.domain([0, 100])
+		.range([margin.left, width - margin.left - margin.right]);
+	$: yScale = scaleLinear()
+		.domain([0, max(data, (d) => d.hours)])
+		.range([height - margin.top - margin.bottom, margin.top]);
+
+	let hoveredData;
 </script>
 
-<main></main>
+<h1>Study longer, score better!</h1>
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+	class="chart-container"
+	bind:clientWidth={width}
+	on:mouseleave={() => {
+		hoveredData = null;
+	}}
+>
+	<svg
+		width={width}
+		height={height}
+	>
+		<AxisX
+			height={height}
+			xScale={xScale}
+			margin={margin}
+		/>
+		<AxisY
+			width={width}
+			yScale={yScale}
+			margin={margin}
+		/>
+		<g
+			class="circles"
+			transform="translate({margin.left}, {margin.top})"
+		>
+			{#each data.sort((a, b) => a.grade - b.grade) as student}
+				<circle
+					cx={xScale(student.grade)}
+					cy={yScale(student.hours)}
+					r={hoveredData && hoveredData == student ? '20' : '10'}
+					fill="purple"
+					opacity={hoveredData ? (hoveredData == student ? '1' : '0.3') : '1'}
+					stroke="black"
+					on:mouseover={() => {
+						hoveredData = student;
+					}}
+					on:focus={() => {
+						hoveredData = student;
+					}}
+					tabIndex="0"
+				/>
+			{/each}
+		</g>
+	</svg>
+	{#if hoveredData}
+		<Tooltip
+			data={hoveredData}
+			xScale={xScale}
+			yScale={yScale}
+		/>
+	{/if}
+</div>
 
 <style>
+	circle {
+		transition: r 300ms ease, opacity 300ms ease;
+		cursor: pointer;
+	}
+
+	circle:focus {
+		outline: none;
+	}
+
+	h1 {
+		padding: 10px;
+		font-size: 2rem;
+        font-family: 'Lato', sans-serif;
+		font-weight: 500;
+		font-style: normal;
+	}
+
+	.chart-container {
+		font-family: 'Lato', sans-serif;
+		font-weight: 400;
+		font-style: normal;
+	}
 </style>
---
+++ src/components/AxisY.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let width, yScale, margin;
+	let yTicks = [0, 20, 40, 60];
+</script>
+
+<g transform="translate({margin.left}, {margin.top})">
+	{#each yTicks as tick}
+		<text
+			x={0}
+			y={yScale(tick)}
+			dy="-6">{tick} {tick == 60 ? ' hours studied' : ''}</text
+		>
+		<line
+			x1="0"
+			y1={yScale(tick)}
+			x2={width}
+			y2={yScale(tick)}
+			stroke="#cecece"
+		></line>
+	{/each}
+</g>
+
+<style>
+	text {
+		font-weight: 300;
+		font-size: 0.9rem;
+	}
+</style>
